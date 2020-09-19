49dc42e37e7265f6dcce0050e157afc1c27f0b78
yogi-rg <yogi@ruangguru.com>
2020-09-20 00:09:12 +0700

    Initial project setup

---
+++ BlindUX/BlindUX/BlindUX.h
@@ -0,0 +1,18 @@
+//
+//  BlindUX.h
+//  BlindUX
+//
+//  Created by Yogi Priyo on 19/09/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlindUX.
+FOUNDATION_EXPORT double BlindUXVersionNumber;
+
+//! Project version string for BlindUX.
+FOUNDATION_EXPORT const unsigned char BlindUXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlindUX/PublicHeader.h>
+
+
---
+++ BlindUX/BlindUXExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlindUXExamples
+//
+//  Created by Yogi Priyo on 19/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
