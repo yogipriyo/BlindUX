06688fb94a810d968399269277bd35565cfd1d60
yogi-rg <yogi@ruangguru.com>
2021-05-31 00:36:30 +0700

    wip-need to migrate workstation

---
+++ BlindUX/BlindUXExamples/Auth/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  BlindUXExamples
+//
+//  Created by Yogi Priyo on 02/12/20.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Private Functions
+    fileprivate func setupNavbar() {
+        self.navigationItem.title = "Login"
+    }
+
+}
